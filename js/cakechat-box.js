e8ad8bf1abf05e74728c3776e9a3aa4bfbad8f1b
Giang <138659940+GiangSandbox@users.noreply.github.com>
2023-12-05 20:55:38 +0700

    Update cakechat-box.js

--- js/cakechat-box.js
+++ js/cakechat-box.js
@@ -1,13 +1,13 @@
 
 //firebase
 var firebaseConfig = {
-    apiKey: "AIzaSyDg4DQR6djcfqH5aRUdhtt4PbkVJGx2Dsk",
-    authDomain: "xproject-f26ed.firebaseapp.com",
-    databaseURL: "https://xproject-f26ed-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "xproject-f26ed",
-    storageBucket: "xproject-f26ed.appspot.com",
-    messagingSenderId: "203552701124",
-    appId: "1:203552701124:web:9569b7db3a15a307f1e791"
+  apiKey: "AIzaSyDg4DQR6djcfqH5aRUdhtt4PbkVJGx2Dsk",
+  authDomain: "xproject-f26ed.firebaseapp.com",
+  databaseURL: "https://xproject-f26ed-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "xproject-f26ed",
+  storageBucket: "xproject-f26ed.appspot.com",
+  messagingSenderId: "203552701124",
+  appId: "1:203552701124:web:9569b7db3a15a307f1e791"
 }
 
 // Initialize Firebase
@@ -30,6 +30,7 @@ var chatBoxToggle = document.getElementById("chat-box-toggle");
 var messageForm = document.getElementById("message-form");
 var messages =  document.getElementById("messages");
 
+//--Email Message
 
 chatCircle.onclick= function(event){
   chatCircle.style.display = "none";
@@ -92,7 +93,36 @@ fetchChat.on("child_added", function (snapshot) {
   
 });
 
+var FormEmail = document.getElementById("form_email_message");
 
+FormEmail.addEventListener("submit", receiveMessage);
+function receiveMessage(e){
+e.preventDefault();
+// get values to be submitted
+const timestamp = Date.now();
+const nameInput = document.getElementById ("name-message");
+const name = nameInput.value;
 
+const emailInput = document.getElementById ("email-message");
+const email = emailInput.value;
 
+const subjectInput = document.getElementById ("subject-message");
+const subject = subjectInput.value;
 
+const bodyInput = document.getElementById ("body-message");
+const body = bodyInput.value;
+
+nameInput. value = "";
+emailInput.value = "";
+subjectInput.value = "";
+bodyInput.value = "";
+
+db.ref("contact-message/" + timestamp).set({
+  name,
+  email,
+  subject,
+  body
+});
+
+alert ("Thank you! Your message has sent us already!");
+}
